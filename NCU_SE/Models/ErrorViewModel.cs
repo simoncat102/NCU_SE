ef432be2bc7c32ead07cd9ad12f829d7baf29fea
simoncat102 <simoncat102@gmail.com>
2021-11-29 19:00:06 +0800

    first commit

---
+++ NCU_SE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NCU_SE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
