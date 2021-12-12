b371955e0d746c810195345976d593bb926dd9c6
Eric <jackbig66@yahoo.com.tw>
2021-12-12 13:04:43 +0800

    註冊功能已完成，請前端協助加上密碼與驗證密碼欄位需相符的js，但目前無法顯示註冊成功訊息

--- NCU_SE/Controllers/UserController.cs
+++ NCU_SE/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Logging;
 using NCU_SE.Data;
 using NCU_SE.Models;
@@ -24,6 +25,8 @@ namespace NCU_SE.Controllers
         */
         //injector
         
+
+
         public UserController(ApplicationDbContext db)
         {
             _db = db;
@@ -34,9 +37,21 @@ namespace NCU_SE.Controllers
         //[ValidateAntiForgeryToken]
         public IActionResult Register(Member obj)
         {
-            _db.Member.Add(obj);
-            _db.SaveChanges();
-            return View();
+            //驗證Email是否已被註冊-->SQL語法轉換 = SELECT count(Email) from Member where Email ='[使用者填寫的Email]'
+            int accountExist = obj.Email == null ? 0: _db.Member.Where(u => u.Email == obj.Email.ToString()).Count();
+            Debug.Print("帳戶存在數量"+accountExist);
+            if(accountExist == 0 && obj.Email!=null)
+            {
+                obj.RegisterDate = System.DateTime.Today;//將註冊日期加入
+                _db.Member.Add(obj);//相當於SQL的insert語法
+                _db.SaveChanges();//儲存資料庫變更
+                return RedirectToAction("Login", "Home");//註冊成功時將導回登入頁面
+            }
+            else if(obj.Email!=null)//需檢驗Email欄位是否為空-->第一次進入是空的
+            {
+                ModelState.AddModelError(nameof(Member.Email), "此帳號已被註冊");//將錯誤訊息附加到欄位上               
+            }
+            return View();           
         }
 
         public IActionResult PersonalInfo()
@@ -53,6 +68,26 @@ namespace NCU_SE.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public int validation(string sql)
+        {
+            /*
+            //測試有沒有連到
+            string connectionstring = configuration.GetConnectionString("DefaultConnection");
+
+            //找到SQLCONNECTION，新增了NuGet套件
+            SqlConnection connection = new SqlConnection(connectionstring);
+            connection.Open();
+            SqlCommand com = new SqlCommand("Select count(*) from Member", connection);
+            var count = (int)com.ExecuteScalar();
+
+            ViewData["TotalData"] = count; //Member資料表的資料數量
+
+            connection.Close(); 
+            */
+            return 0;
+           
         }
     }
 }
---
+++ NCU_SE/Migrations/20211212031728_revertMemberBirthday2Date.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NCU_SE.Migrations
+{
+    public partial class revertMemberBirthday2Date : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Member",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "test",
+                columns: table => new
+                {
+                    MyProperty = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    xx = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_test", x => x.MyProperty);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "test");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Birthday",
+                table: "Member",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
