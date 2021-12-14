5a1c6e43965d82370dd9446bd5df0d950beb0933
Eric <jackbig66@yahoo.com.tw>
2021-12-14 12:32:26 +0800

    以加入登入檢測在需要登入才能使用的頁面、將原本的verify併入Login中、在各controller中加入通用模組(登入檢測/取得session)

--- NCU_SE/Controllers/UserController.cs
+++ NCU_SE/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Logging;
@@ -23,14 +24,7 @@ namespace NCU_SE.Controllers
         }
         */
         //injector
-
-        private readonly ApplicationDbContext _db; //使用資料庫實體
-
-        public UserController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        
+      
 
         //[HttpPost]
         //[ValidateAntiForgeryToken]
@@ -56,6 +50,7 @@ namespace NCU_SE.Controllers
 
         public IActionResult PersonalInfo()
         {
+            if (!LoginStat()) return RedirectToAction("Login", "Home");//若未登入轉跳到登入畫面
             ViewData["login"] = Login_Var.login_status;
             ViewData["logid"] = Login_Var.login_uid;
             return View();
@@ -63,6 +58,7 @@ namespace NCU_SE.Controllers
 
         public IActionResult UserTicket()
         {
+            if (!LoginStat()) return RedirectToAction("Login", "Home");//若未登入轉跳到登入畫面
             ViewData["login"] = Login_Var.login_status;
             ViewData["logid"] = Login_Var.login_uid;
             //讀取資料語法
@@ -75,6 +71,45 @@ namespace NCU_SE.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #region 登入狀態/取得session模組/共用模組
+        //檢測登入狀態
+        public bool LoginStat()
+        {
+            try//檢測session 'acc'是否存在，若存在且不為空則表示已經登入
+            {
+                if (session.HttpContext.Session.GetString("acc") != null)//若已登入
+                {
+                    return true;//跳到首頁
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        //取得Session用的模組==>getSession([Session名稱])==>一律回傳字串，若不存在會傳回null且非字串型態無法取得
+        public string getSession(string name)
+        {
+            string result = null;
+            try
+            {
+                result = session.HttpContext.Session.GetString(name);
+            }
+            catch { }
+            return result;
+        }
+
+        //通用模組
+        private readonly ApplicationDbContext _db; //使用資料庫實體
+        private IHttpContextAccessor session;
+
+        public UserController(ApplicationDbContext db, IHttpContextAccessor httpContextAccessor)
+        {
+            _db = db;
+            session = httpContextAccessor;
+        }
+        #endregion
 
     }
 }
